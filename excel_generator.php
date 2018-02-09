cae14742fd3ea6a3780f0d5542e1b130438af5ab
ChintanNandish <36145715+ChintanNandish@users.noreply.github.com>
2018-02-09 21:47:10 +0530

    Update and rename 123.php to excel_generator.php

--- excel_generator.php
+++ excel_generator.php
@@ -1,5 +1,5 @@
 <?php
-
+session_start();
 require 'vendor/autoload.php';
 
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
@@ -8,13 +8,14 @@ use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 $spreadsheet = new Spreadsheet();
 $sheet = $spreadsheet->getActiveSheet();
 
-$str = file_get_contents('user_folders/chintan_admin/product_data.json');
+$str = file_get_contents('user_folders/'.$_SESSION['username'].'/product_data.json');
 $json = json_decode($str, TRUE);
 $shopname = array_keys($json)[0];
 $product = $json[$shopname];
 $count = 0;
 $keys = array_keys($product[array_keys($product)[0]]);
 unset($keys[array_search('product_image',$keys)]);
+$keys = array_merge(array('product_name'),$keys);
 foreach($keys as $k){
 	$sheet->setCellValue(chr(ord('A')+$count).'1', $k);
 	$count++;
@@ -23,6 +24,7 @@ $count=0;
 $number = 2;
 
 foreach($product as $p){
+	$p = array_merge(array( 'product_name' => array_keys($product)[$count]), $p);
 	unset($p['product_image']);
 	foreach($p as $values){
 		if($values != ''){
@@ -39,7 +41,7 @@ foreach($product as $p){
 
 
 $writer = new Xlsx($spreadsheet);
-$writer->save('user_folders/chintan_admin/'.$shopname.'_product_list.xlsx');
+$writer->save('user_folders/'.$_SESSION['username'].'/'.$shopname.'_product_list.xlsx');
 
 
 ?>
